ac55b58390638b386f9e239b4982bb86932f79e2
Kristian Utsar <78056935+Utsar@users.noreply.github.com>
2021-06-22 15:55:13 +0100

    errorland

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import listEndpoints from "express-list-endpoints";
-import authorsRouter from "./services/authors";
+import authorsRouter from "./services/authors/";
 // import cors from "cors";
 
 const port = 3001;
--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,21 +1,22 @@
-import express from "express";
-import fs from "fs";
-import { fileURLToPath } from "url";
+import express, { response } from "express";
+// import fs from "fs";
+// import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 
 const authorsRouter = express.Router();
 
 authorsRouter.get("/", (request, response) => {
+  console.log("URL", request.url);
   response.send("First try");
 });
 
-authorsRouter.get("/:id");
+// authorsRouter.get("/:id");
 
-authorsRouter.post("/");
+// authorsRouter.post("/");
 
-authorsRouter.put("/:id");
+// authorsRouter.put("/:id");
 
-authorsRouter.delete("/:id");
+// authorsRouter.delete("/:id");
 
 // const currentFilePath = fileURLToPath(import.meta.url);
 // const currentFolderPath = dirname(currentFilePath);
