8562b73e15e5dfda9a1f09ae6b03d25d9a5338ce
Kristian Utsar <78056935+Utsar@users.noreply.github.com>
2021-06-22 14:14:36 +0100

    set-up

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
 import express from "express";
+import listEndpoints from "express-list-endpoints";
+import cors from "cors";
+
+const port = 3001;
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.listen(port, () => {
+  console.log("hello" + port);
+});
