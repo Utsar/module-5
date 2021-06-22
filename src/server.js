8322027da9999babed99102cf9f06ea5d252a6d6
Kristian Utsar <78056935+Utsar@users.noreply.github.com>
2021-06-22 15:30:27 +0100

    server error

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 import express from "express";
 import listEndpoints from "express-list-endpoints";
-import cors from "cors";
+import authorsRouter from "./services/authors";
+// import cors from "cors";
 
 const port = 3001;
 
 const server = express();
 
-server.use(cors());
-server.use(express.json());
+server.use("authors", authorsRouter);
+// server.use(express.json());
+
+console.table(listEndpoints(server));
 
 server.listen(port, () => {
   console.log("hello" + port);
---
+++ src/services/authors/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const authorsRouter = express.Router();
+
+authorsRouter.get("/", (request, response) => {
+  response.send("First try");
+});
+
+authorsRouter.get("/:id");
+
+authorsRouter.post("/");
+
+authorsRouter.put("/:id");
+
+authorsRouter.delete("/:id");
+
+// const currentFilePath = fileURLToPath(import.meta.url);
+// const currentFolderPath = dirname(currentFilePath);
+// const authorsJSONPath = join(currentFilePath, "authors.json");
+
+export default authorsRouter;
