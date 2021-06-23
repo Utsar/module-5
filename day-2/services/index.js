7cdf18babdbfc0f2a5a01077f405c1bdabf3a40e
Kristian Utsar <78056935+Utsar@users.noreply.github.com>
2021-06-23 09:41:14 +0100

    commit

--- day-2/services/index.js
+++ day-2/services/index.js
@@ -2,8 +2,7 @@ import express, { request, response } from "express";
 import fs from "fs";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
-import uniqid from "uniqid"
-
+import uniqid from "uniqid";
 
 const authorsRouter = express.Router();
 
@@ -15,50 +14,53 @@ const currentFolderPath = dirname(currentFilePath);
 const authorsJSONPath = join(currentFilePath, "./authors.json");
 console.log(authorsJSONPath);
 
-const getAuthors = () =>{
-  const content = fs.readFileSync(authorsJSONPath)
-  return JSON.parse(content)
-}
+const getAuthors = () => {
+  const content = fs.readFileSync(authorsJSONPath);
+  return JSON.parse(content);
+};
 
-const writeAuthors = (content) => fs.writeFileSync(authorsJSONPath, JSON.stringify(content))
+const writeAuthors = (content) =>
+  fs.writeFileSync(authorsJSONPath, JSON.stringify(content));
 
 authorsRouter.get("/", (request, response) => {
-  const authors = getAuthors()
-  response.send(authors)
+  const authors = getAuthors();
+  response.send(authors);
 });
 
-authorsRouter.get("/:userid", (request, response) =>{
-
-  const user = user.find(u => u.id === request.params.userid)
-  response.send(authors)
+authorsRouter.get("/:userid", (request, response) => {
+  const user = user.find((u) => u.id === request.params.userid);
+  response.send(authors);
 });
 
 // authorsRouter.post("/");
-authorsRouter.post("/", (request,response) => {
-  const newAuthor = {...request.body, _id: uniqid(), createdAT: new Date()}
-  const authors = getAuthors()
-  authors.push(newAuthor)
-  writeAuthors(authors)
-  response.status(201).send({_id: newAuthor._id})
+authorsRouter.post("/", (request, response) => {
+  const newAuthor = { ...request.body, _id: uniqid(), createdAT: new Date() };
+  const authors = getAuthors();
+  authors.push(newAuthor);
+  writeAuthors(authors);
+  response.status(201).send({ _id: newAuthor._id });
 });
 
 // authorsRouter.put("/:id");
-authorsRouter.put("/:userid", (request, response) =>{
-
+authorsRouter.put("/:userid", (request, response) => {
   const authors = getAuthors();
-  const reminingAuthors = athors.filter(author => author._id !== request.params.userid)
-  const updateAuthor = {...request.body, _id: request.params.userid}
-  reminingAuthors.push(updateAuthor)
-  writeAuthors(reminingAuthors)
-  response.send(updateAuthor)
-}
+  const reminingAuthors = authors.filter(
+    (author) => author._id !== request.params.userid
+  );
+  const updateAuthor = { ...request.body, _id: request.params.userid };
+  reminingAuthors.push(updateAuthor);
+  writeAuthors(reminingAuthors);
+  response.send(updateAuthor);
+});
 
 // authorsRouter.delete("/:id");
-authorsRouter.delete("/:userid", (request,response) => {
-  const authors = getAuthors()
-  const reminingAuthors = authors.filter(author => author._id !== request.params.userid)
-  writeAuthors(reminingAuthors)
-  response.status(204).send()
+authorsRouter.delete("/:userid", (request, response) => {
+  const authors = getAuthors();
+  const reminingAuthors = authors.filter(
+    (author) => author._id !== request.params.userid
+  );
+  writeAuthors(reminingAuthors);
+  response.status(204).send();
 });
 
 export default authorsRouter;
